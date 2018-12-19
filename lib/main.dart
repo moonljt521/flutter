dc05f4eed5a57841536014f93f3027cb91126674
jiangtao.liang <jiangtao.liang@17zuoye.com>
2018-12-19 17:22:06 +0800

    [增加单词收藏功能和路由]

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,13 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     final wordPair = new WordPair.random();
 
     return new MaterialApp(
-
       title: 'Welcome to Flutter',
       home: new Scaffold(
-
         appBar: new AppBar(
           title: new Text('Welcome to Flutter'),
         ),
@@ -34,15 +30,61 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+
   final _suggestions = <WordPair>[];
 
+  // 存储收藏过的单词对 （set）
+  final _saved = new Set<WordPair>();
+
   final TextStyle _biggerFont = new TextStyle(fontSize: 18.0);
+
+  // 导航按钮 click
+  void _pushSaved() {
+
+    print("_pushSaved..........");
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+                (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+
+        // 添加一个 导航按钮
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
+
       ),
       body: _buildSuggestions(),
     );
@@ -63,12 +105,32 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
+  // ！！相当于 adapter ？？？？？
   Widget _buildRow(WordPair pair) {
+
+    // 检测是否已经存储
+    final alreadySaved = _saved.contains(pair);
+
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      // 添加点击事件 ：  setState方法可以触发UI刷新
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+
     );
   }
 }
