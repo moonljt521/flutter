a5f9778e569844b0c6af97316565010806edb7e6
jiangtao.liang <jiangtao.liang@17zuoye.com>
2018-12-29 17:42:25 +0800

    [row and so on]

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_first_demo/HomePage.dart';
+import 'package:flutter_first_demo/HomeWidget.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      // 设置主题
+      theme: new ThemeData(
+        primaryColor: Colors.blueAccent,
+      ),
+      home: HomeWidget(),
+    );
+  }
+}
 
 void main() => runApp(new HomePage());
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,14 +6,14 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_first_demo/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_first_demo/HomePage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new HomePage());
+    await tester.pumpWidget(HomePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
